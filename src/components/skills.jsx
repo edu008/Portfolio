81454826530f0d2018220bc619fbbe04f0ec4008
EduanNR1 <r_eduan@hotmail.com>
2024-11-01 20:37:53 +0100

    UpdateSKills

--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -25,39 +25,45 @@ const skills = () => {
                 <hr className='md:w-44 w-32 md:mb-0 mb-4 h-3 mt-3 md:ml-0 lg:mt-4 bg-gradient-to-r from-[#bf77eced] to-[#c580f0c8]'></hr>
             </div>
             <div>
-                <div className='md:w-full md:h-12 md:mt-10'>
-                    <h1 className='md:font-semibold font-bold md:text-2xl md:pt-1.5 md:inline-block md:mr-5 md:mb-0 mb-1'>Eduan</h1>
-                    <img src={docker} alt="docker" style={{ width: 40, height: 40 }} className='inline md:pb-3 mr-2 md:mr-2'/>
-                    <img src={k8s} alt="k8s" style={{ width: 40, height: 40 }} className='inline md:pb-1 md:mr-2 mr-2 relative md:-top-1.5'/>
-                    <img src={bash} alt="bash" style={{ width: 40, height: 40 }} className='inline md:pb-1 md:mb-3 mr-2 md:mr-2'/>
-                    <img src={selenium} alt="selenium" style={{ width: 45, height: 45 }} className='inline md:pb-4 mr-2 md:mr-2'/>
-                </div>
-                <div>
-                    <h1 className='md:font-semibold font-bold md:text-2xl md:pt-2 md:inline-block md:mr-5 md:mb-0 mb-1'>Web Dev</h1>
-                    <img src="https://skillicons.dev/icons?i=react" alt="react" style={{ width: 45, height: 45 }} className='inline md:pb-3 mr-2 md:mr-2 md:mb-0 mb-2 md:mt-0 mt-2'/>
-                    <img src={tailwind} alt="tailwind" style={{ width: 45, height: 45 }} className='inline md:pb-3 md:mr-1 mr-2'/>
-                    <img src={mongo} alt="mongodb" style={{ width: 45, height: 45 }} className='inline md:pb-3 md:mr-0.5 mr-2'/>
-                    <img src='https://skillicons.dev/icons?i=html' alt="html5" style={{ width: 45, height: 45 }} className='mr-2 inline md:pb-3 md:mr-1'/>
-                    <img src='https://skillicons.dev/icons?i=nodejs' alt="html5" style={{ width: 45, height: 45 }} className='mr-2 inline md:pb-3 md:mr-1'/>
-                    <img src='https://skillicons.dev/icons?i=js' alt="js" style={{ width: 45, height: 45 }} className='inline md:pb-3 mr-2 md:mr-1 md:mt-0 '/>
-                    <img src='https://skillicons.dev/icons?i=mysql' alt="mysql" style={{ width: 45, height: 45 }} className='inline md:pb-3 mr-2 md:mr-1 md:mt-0'/>
-                </div>
-                <div>
-                    <h1 className='md:font-semibold font-bold md:text-2xl md:pt-1.5 md:inline-block md:mr-5 md:mb-0 mb-2.5 md:mt-0 mt-2'>Languages</h1>
-                    <img src='https://skillicons.dev/icons?i=c' alt="c" style={{ width: 45, height: 45 }} className='inline md:pb-3 md:mr-1 mr-2'/>
-                    <img src='https://skillicons.dev/icons?i=cpp' alt="c++" style={{ width: 45, height: 45 }} className='inline md:pb-3 md:mr-1 mr-2'/>
-                    <img src='https://skillicons.dev/icons?i=python&theme=dark' alt="python" style={{ width: 45, height: 45 }} className='inline md:pb-3 mr-2 md:mr-1'/>
-                    <img src='https://skillicons.dev/icons?i=java&theme=light' alt="java" style={{ width: 45, height: 45 }} className='inline md:pb-3 mr-2 md:mr-1'/>
-                    <img src='https://skillicons.dev/icons?i=golang' alt="golang" style={{ width: 45, height: 45 }} className='inline md:pb-3 md:mr-1'/>
-                </div>
-                <div>
-                    <h1 className='md:font-semibold font-bold md:text-2xl md:pt-1.5 md:inline-block md:mr-5 md:mb-0 mb-2 md:mt-0 mt-2'>Others Skills</h1>
-                    <img src='https://skillicons.dev/icons?i=git' alt="git" style={{ width: 45, height: 45 }} className='inline md:pb-3 md:mr-1 mr-2'/>
-                    <img src='https://skillicons.dev/icons?i=github' alt="github" style={{ width: 45, height: 45 }} className='inline md:pb-3 md:mr-1 mr-2'/>
-                    <img src='https://skillicons.dev/icons?i=vim' alt="vim" style={{ width: 45, height: 45 }} className='inline md:pb-3 md:mr-1 mr-2'/>
-                    <img src='https://skillicons.dev/icons?i=flask' alt="vim" style={{ width: 45, height: 45 }} className='inline md:pb-3 md:mr-1 mr-2'/>
-                    <img src='https://skillicons.dev/icons?i=linux&theme=dark' alt="linux" style={{ width: 45, height: 45 }} className='inline md:pb-3 md:mr-1 mr-2'/>
-                </div>
+<div className='md:w-full md:h-12 md:mt-10'>
+    <h1 className='md:font-semibold font-bold md:text-2xl md:pt-1.5 md:inline-block md:mr-5 md:mb-0 mb-1'>Software Tools and Platforms</h1>
+    <img src="https://skillicons.dev/icons?i=figma" alt="figma" style={{ width: 45, height: 45 }} className='inline md:pb-3 mr-2'/>
+    <img src="https://skillicons.dev/icons?i=azure" alt="azure" style={{ width: 45, height: 45 }} className='inline md:pb-3 mr-2'/>
+    <img src="https://skillicons.dev/icons?i=visualstudio" alt="visualstudio" style={{ width: 45, height: 45 }} className='inline md:pb-3 mr-2'/>
+    <img src="https://skillicons.dev/icons?i=selenium" alt="selenium" style={{ width: 45, height: 45 }} className='inline md:pb-4 mr-2'/>
+    <img src="https://skillicons.dev/icons?i=vscode" alt="vscode" style={{ width: 45, height: 45 }} className='inline md:pb-3 mr-2'/>
+    <img src="https://skillicons.dev/icons?i=github" alt="github" style={{ width: 45, height: 45 }} className='inline md:pb-3 mr-2'/>
+</div>
+
+<div>
+    <h1 className='md:font-semibold font-bold md:text-2xl md:pt-2 md:inline-block md:mr-5 md:mb-0 mb-1'>Databases and Data Tools</h1>
+    <img src="https://skillicons.dev/icons?i=mongodb" alt="mongodb" style={{ width: 45, height: 45 }} className='inline md:pb-3 mr-2'/>
+    <img src="https://skillicons.dev/icons?i=mysql" alt="mysql" style={{ width: 45, height: 45 }} className='inline md:pb-3 mr-2'/>
+</div>
+
+<div>
+    <h1 className='md:font-semibold font-bold md:text-2xl md:pt-1.5 md:inline-block md:mr-5 md:mb-0 mb-2.5 md:mt-0 mt-2'>Programming Languages and Frameworks</h1>
+    <img src="https://skillicons.dev/icons?i=python" alt="python" style={{ width: 45, height: 45 }} className='inline md:pb-3 mr-2'/>
+    <img src="https://skillicons.dev/icons?i=javascript" alt="javascript" style={{ width: 45, height: 45 }} className='inline md:pb-3 mr-2'/>
+    <img src="https://skillicons.dev/icons?i=nodejs" alt="nodejs" style={{ width: 45, height: 45 }} className='inline md:pb-3 mr-2'/>
+    <img src="https://skillicons.dev/icons?i=angular" alt="angular" style={{ width: 45, height: 45 }} className='inline md:pb-3 mr-2'/>
+    <img src="https://skillicons.dev/icons?i=css" alt="css" style={{ width: 45, height: 45 }} className='inline md:pb-3 mr-2'/>
+    <img src="https://skillicons.dev/icons?i=html" alt="html" style={{ width: 45, height: 45 }} className='inline md:pb-3 mr-2'/>
+</div>
+
+<div>
+    <h1 className='md:font-semibold font-bold md:text-2xl md:pt-1.5 md:inline-block md:mr-5 md:mb-0 mb-2 md:mt-0 mt-2'>Methods and Techniques</h1>
+    <img src="https://skillicons.dev/icons?i=code" alt="programming" style={{ width: 45, height: 45 }} className='inline md:pb-3 mr-2'/>
+</div>
+
+<div>
+    <h1 className='md:font-semibold font-bold md:text-2xl md:pt-1.5 md:inline-block md:mr-5 md:mb-0 mb-2 md:mt-0 mt-2'>General Skills and Abilities</h1>
+    <img src="https://skillicons.dev/icons?i=office" alt="office" style={{ width: 45, height: 45 }} className='inline md:pb-3 mr-2'/>
+    <img src="https://skillicons.dev/icons?i=excel" alt="excel" style={{ width: 45, height: 45 }} className='inline md:pb-3 mr-2'/>
+</div>
+
+
+
             </div>
         </div>
         <div className='hidden lg:block'>
